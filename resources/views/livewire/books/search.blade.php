5cc02da12802666bd65bb703af7aec4c0b5c6467
ishini-gw <ishini.dewamiththa@gateway.lk>
2026-05-05 01:48:49 +0530

    remove search files

--- resources/views/livewire/books/search.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <input 
-        type="text" 
-        wire:model.live.debounce.1000ms="search"
-        placeholder="Search classrooms..."
-        class="w-[90%] mx-auto block px-4 py-2 mt-10 border rounded-lg"
-    >
-
-</div>
